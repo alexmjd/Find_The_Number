62aa9b08d7f784bff08d3563989e0d95692cd962
alexandremajda <majda.a@outlook.com>
2020-12-13 14:40:40 +0100

    [Feature] Implements `Find the Number` algorithm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int propal(0);
+    int numberToFind(32781);
+
+    /*
+     * While the user hasn't found the correct number yet
+     * the user has to type the number he thinks is correct
+     */
+    while (propal != numberToFind) {
+        std::cout << "Enter the number you think is correct : " << std::endl;
+        std::cin >> propal;
+        if (propal < numberToFind)
+            std::cout << "It's more." << std::endl;
+        else if (propal > numberToFind)
+            std::cout << "It's less." << std::endl;
+    }
+
+    std::cout << "Congrats ! You Win!" << std::endl;
+
     return 0;
 }
