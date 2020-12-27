d1e302b22d130d45f1739be650b3f7969ae8b401
alexandremajda <majda.a@outlook.com>
2020-12-27 22:29:24 +0100

    [Feature] Setting properties to buttons for choosing level to make them responsive.

--- main.cpp
+++ main.cpp
@@ -10,23 +10,30 @@ int main() {
 
     // Create the window instance
     sf::RenderWindow window;
-    window.create(sf::VideoMode(800, 600), "Find The Number");
+    window.create(sf::VideoMode(1200, 700), "Find The Number");
 
     // Set FPS limit to avoid overwarm of the computer (and because it's useless too)
     window.setFramerateLimit(60);
 
+    float widthRect = window.getSize().x / 4;
+    float heightRect = 150.f;
+
+
     // Declare 3 rects for choosing the difficulty level
-    sf::RectangleShape rect(sf::Vector2(150.f, 150.f));
-    sf::RectangleShape rect2(sf::Vector2(150.f, 150.f));
-    sf::RectangleShape rect3(sf::Vector2(150.f, 150.f));
+    sf::RectangleShape rect(sf::Vector2(widthRect, heightRect));
+    sf::RectangleShape rect2(sf::Vector2(widthRect, heightRect));
+    sf::RectangleShape rect3(sf::Vector2(widthRect, heightRect));
 
     // Set options for rects
     rect.setOrigin(rect.getSize().x / 2, rect.getSize().y / 2);
-    rect.setPosition(rect.getSize().x, window.getSize().y / 2);
+    rect.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+
     rect2.setOrigin(rect2.getSize().x / 2, rect2.getSize().y / 2);
-    rect2.setPosition(rect2.getSize().x * 2 + 20, window.getSize().y / 2);
+    rect2.setPosition(rect.getPosition().x / 2 - 20, window.getSize().y / 2);
+
     rect3.setOrigin(rect3.getSize().x / 2, rect3.getSize().y / 2);
-    rect3.setPosition(rect3.getSize().x * 3 + 40, window.getSize().y / 2);
+    rect3.setPosition(20 + rect.getPosition().x + rect.getPosition().x / 2,
+                      window.getSize().y / 2);
 
     // Keep the window open while the window event type isn't `close`
     while(window.isOpen()) {
