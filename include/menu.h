2b0eafc9bb641da71474b95c4ec23f791f5d10c6
alexandremajda <majda.a@outlook.com>
2020-12-28 02:56:20 +0100

    [Cleanse] Factorising the menu.

--- include/menu.h
+++ include/menu.h
@@ -10,23 +10,19 @@
 class Menu {
 
 private:
-    sf::RectangleShape _rect;
-    sf::RectangleShape _rect2;
-    sf::RectangleShape _rect3;
+    std::vector<sf::RectangleShape> _menuElements;
 
     float _widthRect = 250.f;
     float _heightRect = 150.f;
 
 public:
-
     void setRectangles(float windowX, float windowY);
 
-    sf::RectangleShape getRectangle1();
-    sf::RectangleShape getRectangle2() const;
-    sf::RectangleShape getRectangle3() const;
+    void setRectanglesOrigines();
+
+    void setRectanglesPosition(float windowX, float windowY);
 
     void draw(sf::RenderWindow &window);
 };
-
 
 #endif //FIND_THE_NUMBER_MENU_H
